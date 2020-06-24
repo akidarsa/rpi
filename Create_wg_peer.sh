850aaac43d82979d7fc9679e9a006e20eee610c0
akidarsa <akidarsa@gmail.com>
2020-06-25 01:46:41 +0800

    Create Create_wg_peer.sh

---
+++ Create_wg_peer.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+##### Check what is the latest number in the Peer #####
+number=0
+number=`ls *.conf | cut -d "." -f 1 | cut -c 5-10 | sort -n | tail -n 1`
+number=$((number+1))
+if [ "$number" -lt "7" ]; then
+        number=7
+fi
+echo "Peer Number is $number"
+
+##### Variable for where the files will be #####
+scon="/etc/wireguard/wg0.conf"
+pcon="/etc/wireguard/peer$number.conf"
+port="8988"
+pubip="aksg.duckdns.org"
+spubk=`cat /etc/wireguard/server_publickey`
+dnsaddr="192.168.1.245"
+
+##### Generate the actual keys#####
+wg genkey | tee peer"$number"_privatekey | wg pubkey > peer"$number"_publickey
+
+##### Load the keys to variables to write into the files #####
+pubk=`cat peer"$number"_publickey`
+prik=`cat peer"$number"_privatekey`
+
+##### Write the configuration to the Server Config File #####
+echo "[Peer]" >> $scon
+echo "#Peer-$number" >> $scon
+echo "PublicKey = $pubk" >> $scon
+echo "AllowedIPs = 10.10.10.$number/32" >> $scon
+echo "#PersistentkeepAlive = 60" >> $scon
+echo "" >> $scon
+
+##### Write the configuration to the Peer Config File #####
+echo "[Interface]" > $pcon
+echo "Address = 10.10.10."$number"/32" >> $pcon
+echo "DNS = $dnsaddr" >> $pcon
+echo "PrivateKey = $prik" >> $pcon
+echo "" >> $pcon
+echo "[Peer]" >> $pcon
+echo "PublicKey = $spubk" >> $pcon
+echo "Endpoint = $pubip:$port" >> $pcon
+echo "AllowedIPs = 0.0.0.0/0, ::/0" >> $pcon
+echo "#PersistentkeepAlive = 60" >> $pcon
+
+##### Create the QR code for Phone Peer #####
+qrencode -t ansiutf8 < $pcon
